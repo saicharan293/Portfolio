cd61e6559ac63df84614afb68a6189a7a92fe5ed
saicharan293 <2493saicharan@gmail.com>
2024-11-26 23:37:05 +0530

    Changes are done

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,40 +3,98 @@ import { BiLogoFlask, BiLogoPostgresql, BiLogoTailwindCss } from 'react-icons/bi
 import { FaCss3Alt, FaHtml5, FaNodeJs, FaPython } from 'react-icons/fa'
 import { RiReactjsLine } from 'react-icons/ri'
 import { SiMongodb } from 'react-icons/si'
+import {animate, motion} from 'framer-motion'
+
+const iconVariants=(duration)=>({
+    initial: {y: -10},
+    animate:{
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: 'linear',
+            repeat: Infinity,
+            repeatType: "reverse"
+        }
+    }
+})
 
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
-        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.h2 
+            whileInView={{opacity: 1, y: 0}}
+            initial={{opacity: 0, y: -100}}
+            transition={{ duration: 1.5 }}
+            className='my-20 text-center text-4xl'>Technologies</motion.h2>
+        <motion.div 
+            whileInView={{opacity: 1, x: 0}}
+            initial={{opacity: 0, x: -100}}
+            transition={{duration: 1.5}}
+            className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiMongodb className='text-7xl text-green-500'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaNodeJs className='text-7xl text-green-500'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <BiLogoPostgresql className='text-7xl text-sky-700'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(6)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaPython className='text-7xl text-[#3f7baa]'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <BiLogoFlask className='text-7xl text-gray-400'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(6)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaHtml5 className='text-7xl text-orange-600'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaCss3Alt className='text-7xl text-blue-400'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <BiLogoTailwindCss className='text-7xl text-blue-600'/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,39 +3,20 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React , as well as back-end technologies like Node.js, Python, MySQL, PostgreSQL, and MongoDB.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a motivated and adaptable full stack developer with 9 months of hands-on experience building efficient and user-friendly web applications. I have worked extensively with technologies like React, Node.js, MySQL, PostgreSQL, and MongoDB, enabling me to develop robust and scalable solutions.
+My journey into web development was driven by a strong curiosity to understand the mechanics behind web technologies.`
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
     year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    role: "Associate Software Engineer",
+    company: "Hexaware Technologies Limited.",
+    description: `Worked as Frontend Web Developer to build applications using JavaScript, React.js, and Springboot. Implemented RESTful APIs and integrated with MySql databases.`,
+    technologies: ["Javascript", "React.js", "Springboot", "MySQL"],
   },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+  
 ];
 
 export const PROJECTS = [
