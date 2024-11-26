d5f60b7237cfe614c96d92d0215a50dd6462aaf0
saicharan293 <2493saicharan@gmail.com>
2024-11-26 20:34:04 +0530

    about me page started

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='border-b border-neutral-900 pb-4'>
+      <h2 className="my-20 text-center text-4xl">About Me</h2>
+    </div>
+  )
+}
+
+export default About
