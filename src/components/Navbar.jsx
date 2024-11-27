bfad88b3b9c6581efae2f5edd100da250df0374d
saicharan293 <2493saicharan@gmail.com>
2024-11-27 10:54:54 +0530

    links for navbar are added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,15 @@ const Navbar = () => {
             <img className='mx-2 w-10' src={logo} alt="logo" />
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+          <a href="https://www.linkedin.com/in/amudala-sai-charan-14860a17b/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin />
-            <FaGithub />
-            <FaInstagram/>
+          </a>
+          <a href="https://github.com/saicharan293" target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+          </a>
+          <a href="https://www.instagram.com/saicharan_amudala/" target="_blank" rel="noopener noreferrer">
+              <FaInstagram/>
+          </a>
         </div>
     </nav>
   )
